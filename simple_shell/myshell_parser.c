3ec6bf538e47711bafe5325f2574e228da6ed2a4
jullianzz <jzeng8827@bths.edu>
2022-01-25 00:52:02 -0500

    Went back to original method. Tokens data structure getting closer. Still debugging.

--- simple_shell/myshell_parser.c
+++ simple_shell/myshell_parser.c
@@ -5,7 +5,9 @@
 #include "stdlib.h"
 
 
-void add_to_tokens(char *beg, char *end, char *tokens_ds[], int *rtc) {
+void split_symbols(char *beg, char *end, char *tokens_ds[], int *rtc) {
+
+	// printf("Beginning is %s\n", beg);
 
 	if (beg == end) {	// beg == end if they both point to a &, |, <, or > symbol
 		char *hptr = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer
@@ -14,8 +16,8 @@ void add_to_tokens(char *beg, char *end, char *tokens_ds[], int *rtc) {
 		symbol[1] = '\0';
 		strcpy(hptr, symbol);
 		tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
-		// printf("symbol is %s\n", symbol);
-		printf("rtc is %d\n", *rtc);
+		printf("LOC 1: heap pointer is %s\n", hptr);
+		// printf("rtc is %d\n", *rtc)/;
 		printf("Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 				// Increment rtc by 1
 	} 
@@ -33,11 +35,14 @@ void add_to_tokens(char *beg, char *end, char *tokens_ds[], int *rtc) {
 		* into a char array called symbol
 		*/ 
 		char *hptr = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer
-		char symbol[2];
-		symbol[0] = *end; 
-		symbol[1] = '\0';
-		strcpy(hptr, symbol);
+		hptr = NULL;
 
+		if (*end != '\0') {
+			char symbol[2];
+			symbol[0] = *end; 
+			symbol[1] = '\0';
+			strcpy(hptr, symbol);
+		}
 		/*
 		* Add word and symbol to tokens_ds
 		*/
@@ -45,29 +50,36 @@ void add_to_tokens(char *beg, char *end, char *tokens_ds[], int *rtc) {
 		char *hptr_beg = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer. NOTE: length = 1 here, this could cause problems
 		strcpy(hptr_beg, beg); 
 		tokens_ds[*rtc] = hptr_beg; 			// Add beg to tokens_ds
-		printf("rtc is %d\n", *rtc);
+		printf("LOC 2: heap pointer is %s\n", hptr_beg);
+		// printf("rtc is %d\n", *rtc);
 		printf("Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 				// Increment rtc by 1
-		tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
-		printf("rtc is %d\n", *rtc);
-		printf("Token added: %s\n", tokens_ds[*rtc]);
-		*rtc = *rtc + 1; 				// Increment rtc by 1
+		if (hptr != NULL) {
+			tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
+			printf("LOC 3: heap pointer is %s\n", hptr);
+			// printf("rtc is %d\n", *rtc);
+			printf("Token added: %s\n", tokens_ds[*rtc]);
+			*rtc = *rtc + 1; 				// Increment rtc by 1
+		}
 	}
 }
 
 
-void tokenize_symbols(char ptr[], char *tokens_ds[], int *rtc) 
+void tokenize(char ptr[], char *tokens_ds[], int *rtc) 
 {
 	/*
 	* Check if any of &, |, <, or > symbols exist
 	*/ 
+
+	printf("\nTokenizing %s\n", ptr); 
 
 	if (strchr(ptr, '&') == NULL && strchr(ptr, '|') == NULL && strchr(ptr, '<') == NULL && strchr(ptr, '>') == NULL) {
 		// If none of the symbols exist, add ptr to tokens_ds and return
 		char *hptr = malloc(sizeof *hptr * strlen(ptr));
 		strcpy(hptr, ptr); 
 		tokens_ds[*rtc] = hptr; 
-		printf("rtc is %d\n", *rtc);
+		printf("LOC 4: heap pointer is %s\n", hptr);
+		// printf("rtc is %d\n", *rtc);
 		printf("Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 		// Increment rtc by 1
 		return; 
@@ -83,9 +95,10 @@ void tokenize_symbols(char ptr[], char *tokens_ds[], int *rtc)
 		end = ptr; 
 
 		while (*end != '\0') {
+			// printf("End is %s\n", end);
 			if (*end == '&' || *end == '|' || *end == '>' || *end == '<') {
 				// TODO: problem lies here. The program doesn't come here even though it should??
-				add_to_tokens(beg, end, tokens_ds, rtc); 
+				split_symbols(beg, end, tokens_ds, rtc); 
 				end = end + 1; 
 				beg = end; 
 			} else
@@ -93,7 +106,7 @@ void tokenize_symbols(char ptr[], char *tokens_ds[], int *rtc)
 		}
 
 		if (*beg != '\0') {
-			add_to_tokens(beg, end, tokens_ds, rtc); 
+			split_symbols(beg, end, tokens_ds, rtc); 
 		}
 
 	}
@@ -108,15 +121,14 @@ struct pipeline *pipeline_build(const char *command_line)
 	* Initialize array of char pointers (tokens_ds) to point to tokens and
 	* allocate maximum possible memory needed
 	*/
-	// char **tokens_ds = (char **) malloc(cmdl_len * sizeof(char *));
 	char **tokens_ds = malloc(sizeof **tokens_ds * cmdl_len);
 
 	/*
 	* Make non-const copy of command_line for the purpose of using with strtok which 
 	* modifies the string parameter passed to it
 	*/
-	char cmdl_cpy[cmdl_len]; 					// Initialize char[] of same length as command_line
-	strcpy(cmdl_cpy, command_line); 			// Copy command_line into cmdl_cpy
+	char cmdl_cpy[cmdl_len]; 			
+	strcpy(cmdl_cpy, command_line);
 	
 	/* 
 	* Split cmdl_cpy by whitespace by calling strtok iteratively until the strtok's 
@@ -126,25 +138,24 @@ struct pipeline *pipeline_build(const char *command_line)
 	char *ptr = strtok(cmdl_cpy, delim); 		// Make first call to strtok with parameters cmdl_cpy and delim 
 	while (ptr != NULL) {
 		/*
-		* Call tokenize_symbols on ptr which checks if any symbols &, |, <, or > exist
+		* Call tokenize on ptr which checks if any symbols &, |, <, or > exist
 		* in ptr, and if so add them to tokens_ds
 		*/
-		printf("%s\n", ptr);
+		// printf("%s\n", ptr);
 		char ptr_cpy[strlen(ptr)]; 
 		strcpy(ptr_cpy, ptr);
-		tokenize_symbols(ptr_cpy, tokens_ds, &rtc); 
-		printf("Token[0] added: %s\n", tokens_ds[0]);
-		printf("Token[1] added: %s\n", tokens_ds[1]);
-		printf("Token[2] added: %s\n", tokens_ds[2]);
+		tokenize(ptr_cpy, tokens_ds, &rtc); 
+		// printf("tokens[0] = %s\n", tokens_ds[0]);
+		// printf("tokens[1] = %s\n", tokens_ds[1]);
 		ptr = strtok(NULL, delim);
 	}
 
+	printf("\nPRINT TOKENS DATA STRUCTURE:\n");
+
+	for (int i = 0; i < rtc; i++) {
+		printf("%s\n", tokens_ds[i]);
+	}
 	printf("\n");
-	// printf("YASSS Token added: %s\n", tokens_ds[0]);
-	// for (int i = 0; i < sizeof(tokens_ds)/sizeof(tokens_ds[0]); i++) {
-	// 	printf("%s\n", tokens_ds[i]);
-	// }
-	// printf("\n");
 
 	/*
 	* Shrink size of tokens_ds
@@ -161,42 +172,10 @@ void pipeline_free(struct pipeline *pipeline)
 	// TODO: Implement this function
 }
 
-void rahrah(char *tokens_ds[]) {
-	// char *ptr; 
-	char str[] = "jelly";
-	// ptr = str; 
-
-	char *ptr = malloc(sizeof *ptr * 1); 
-	strcpy(ptr, str);
-	tokens_ds[0] = ptr; 
-	printf("Inside rahrah: tokens_ds is %s\n", tokens_ds[0]); 
-}
-
-struct pipeline *pipeline_build_too(const char *command_line) {
-
-	return NULL; 
-}
 
 int main() {
 	// code here to test pipeline_build
-	// pipeline_build("julia <zeng"); 
-
-	// correct output: 
-	// julia
-	// <
-	// zeng
-
-	char **tokens_ds = malloc(sizeof **tokens_ds * 1);
-	char *ptr = malloc(sizeof *ptr * 1); 
-	char str[] = "julia"; 
-	strcpy(ptr, str);
-	printf("ptr is %s\n", ptr);
-	char str_too[] = "zeng";
-	strcpy(ptr, str_too);
-	printf("ptr is %s\n", ptr);
-	// printf("Before rahrah: tokens_ds is %s\n", tokens_ds[0]); 
-	// rahrah(tokens_ds);
-	// printf("After rahrah: tokens_ds is %s\n", tokens_ds[0]); 
+	pipeline_build("Julia |Zeng >Chicken"); 
 
 }
 
