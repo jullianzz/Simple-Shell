bf5bee6cd7be6376e34fea017218d9a9d0b9f64c
jullianzz <jzeng8827@bths.edu>
2022-01-25 02:16:00 -0500

    Fixed partial bugs. Currently lexing test cases 2 and 3 successfully, this is a good sign. Failing on test case 1 when a symbol immediately succeeds a word.

--- simple_shell/myshell_parser.c
+++ simple_shell/myshell_parser.c
@@ -10,15 +10,15 @@ void split_symbols(char *beg, char *end, char *tokens_ds[], int *rtc) {
 	// printf("Beginning is %s\n", beg);
 
 	if (beg == end) {	// beg == end if they both point to a &, |, <, or > symbol
-		char *hptr = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer
+		char *hptr = (char *) malloc(sizeof(char) * 2);  	// hptr stands for heap pointer
 		char symbol[2];
 		symbol[0] = *end; 
 		symbol[1] = '\0';
 		strcpy(hptr, symbol);
 		tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
-		printf("LOC 1: heap pointer is %s\n", hptr);
+		// printf("LOC 1: heap pointer is %s\n", hptr);
 		// printf("rtc is %d\n", *rtc)/;
-		printf("Token added: %s\n", tokens_ds[*rtc]);
+		printf("LOC 1: Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 				// Increment rtc by 1
 	} 
 	
@@ -34,7 +34,7 @@ void split_symbols(char *beg, char *end, char *tokens_ds[], int *rtc) {
 		* Copy symbol (will be &, |, <, >, or NUL) end is pointing to 
 		* into a char array called symbol
 		*/ 
-		char *hptr = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer
+		char *hptr = (char *) malloc(sizeof(char)* 2);  	// hptr stands for heap pointer
 		hptr = NULL;
 
 		if (*end != '\0') {
@@ -47,18 +47,18 @@ void split_symbols(char *beg, char *end, char *tokens_ds[], int *rtc) {
 		* Add word and symbol to tokens_ds
 		*/
 		*end = '\0'; 					// Make end NUL to give the word a NUL terminator
-		char *hptr_beg = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer. NOTE: length = 1 here, this could cause problems
+		char *hptr_beg = (char *) malloc(sizeof(char) * (strlen(beg) + 1));  	// hptr stands for heap pointer. NOTE: length = 1 here, this could cause problems
 		strcpy(hptr_beg, beg); 
 		tokens_ds[*rtc] = hptr_beg; 			// Add beg to tokens_ds
-		printf("LOC 2: heap pointer is %s\n", hptr_beg);
+		// printf("LOC 2: heap pointer is %s\n", hptr_beg);
 		// printf("rtc is %d\n", *rtc);
-		printf("Token added: %s\n", tokens_ds[*rtc]);
+		printf("LOC 2: Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 				// Increment rtc by 1
 		if (hptr != NULL) {
 			tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
-			printf("LOC 3: heap pointer is %s\n", hptr);
+			// printf("LOC 3: heap pointer is %s\n", hptr);
 			// printf("rtc is %d\n", *rtc);
-			printf("Token added: %s\n", tokens_ds[*rtc]);
+			printf("LOC 3: Token added: %s\n", tokens_ds[*rtc]);
 			*rtc = *rtc + 1; 				// Increment rtc by 1
 		}
 	}
@@ -68,19 +68,21 @@ void split_symbols(char *beg, char *end, char *tokens_ds[], int *rtc) {
 void tokenize(char ptr[], char *tokens_ds[], int *rtc) 
 {
 	/*
-	* Check if any of &, |, <, or > symbols exist
+	* Check if any of &, |, <, or > symbols exist.
+	* If they don't just add ptr to tokens_ds.
+	* If they do, continue tokenizing.
 	*/ 
 
 	printf("\nTokenizing %s\n", ptr); 
 
 	if (strchr(ptr, '&') == NULL && strchr(ptr, '|') == NULL && strchr(ptr, '<') == NULL && strchr(ptr, '>') == NULL) {
 		// If none of the symbols exist, add ptr to tokens_ds and return
-		char *hptr = malloc(sizeof *hptr * strlen(ptr));
+		char *hptr = (char *) malloc(sizeof(char) * (strlen(ptr) + 1));
 		strcpy(hptr, ptr); 
 		tokens_ds[*rtc] = hptr; 
-		printf("LOC 4: heap pointer is %s\n", hptr);
+		// printf("LOC 4: heap pointer is %s\n", hptr);
 		// printf("rtc is %d\n", *rtc);
-		printf("Token added: %s\n", tokens_ds[*rtc]);
+		printf("LOC 4: Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 		// Increment rtc by 1
 		return; 
 	} 
@@ -114,14 +116,14 @@ void tokenize(char ptr[], char *tokens_ds[], int *rtc)
 
 struct pipeline *pipeline_build(const char *command_line)
 {
-	const int cmdl_len = strlen(command_line); 	// Find size of command_line string
+	const int cmdl_len = strlen(command_line) + 1; 	// Find size of command_line string
 	int rtc = 0; 	// rtc stands for running tokens count, it increments by 1 after a token pointer is added to tokens_ds
 
 	/*
 	* Initialize array of char pointers (tokens_ds) to point to tokens and
 	* allocate maximum possible memory needed
 	*/
-	char **tokens_ds = malloc(sizeof **tokens_ds * cmdl_len);
+	char **tokens_ds = (char **) malloc(sizeof(char *) * cmdl_len);
 
 	/*
 	* Make non-const copy of command_line for the purpose of using with strtok which 
@@ -141,12 +143,10 @@ struct pipeline *pipeline_build(const char *command_line)
 		* Call tokenize on ptr which checks if any symbols &, |, <, or > exist
 		* in ptr, and if so add them to tokens_ds
 		*/
-		// printf("%s\n", ptr);
-		char ptr_cpy[strlen(ptr)]; 
-		strcpy(ptr_cpy, ptr);
-		tokenize(ptr_cpy, tokens_ds, &rtc); 
+		tokenize(ptr, tokens_ds, &rtc); 
 		// printf("tokens[0] = %s\n", tokens_ds[0]);
 		// printf("tokens[1] = %s\n", tokens_ds[1]);
+		// printf("tokens[2] = %s\n", tokens_ds[2]);
 		ptr = strtok(NULL, delim);
 	}
 
@@ -175,7 +175,9 @@ void pipeline_free(struct pipeline *pipeline)
 
 int main() {
 	// code here to test pipeline_build
-	pipeline_build("Julia |Zeng >Chicken"); 
+	pipeline_build("Julia |Zeng Chicken>"); // test case 1
+	// pipeline_build("Julia < Zeng < Chickens |yar");  // test case 2
+	// pipeline_build("Julia Zeng Chickens Yas"); // test case 3
 
 }
 
