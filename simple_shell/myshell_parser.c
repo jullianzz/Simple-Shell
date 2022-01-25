245b4c23207ed4448d9988a7979e6395f020bb3d
jullianzz <jzeng8827@bths.edu>
2022-01-24 23:27:06 -0500

    So,,, my code is a bit of a mess right now, but I'm trying out a different algorithm for the lexing, the original was too complicated. Commit to show progress.

--- simple_shell/myshell_parser.c
+++ simple_shell/myshell_parser.c
@@ -6,13 +6,53 @@
 
 
 void add_to_tokens(char *beg, char *end, char *tokens_ds[], int *rtc) {
-	char end_cpy[strlen(end)]; 
-	strcpy(end_cpy, end); 			// Make copy of end
-	*end = '\0'; 					// Make end a null character
-	tokens_ds[*rtc] = beg; 			// Add beg to tokens_ds
-	*rtc = *rtc + 1; 				// Increment rtc by 1
-	tokens_ds[*rtc] = end_cpy; 		// Add end copy to tokens_ds
-	// Now beg points at original beg, end points at null character
+
+	if (beg == end) {	// beg == end if they both point to a &, |, <, or > symbol
+		char *hptr = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer
+		char symbol[2];
+		symbol[0] = *end; 
+		symbol[1] = '\0';
+		strcpy(hptr, symbol);
+		tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
+		// printf("symbol is %s\n", symbol);
+		printf("rtc is %d\n", *rtc);
+		printf("Token added: %s\n", tokens_ds[*rtc]);
+		*rtc = *rtc + 1; 				// Increment rtc by 1
+	} 
+	
+	/* 
+	* If the program reaches this point, the set of characters starting  
+	* from the beg pointer and ending at the end pointer are either: (1) one word
+	* followed by a symbol (pointed to by end), or (2) one word followed by a 
+	* NUL (pointed to by end).
+	*/
+
+	else { 			
+		/*
+		* Copy symbol (will be &, |, <, >, or NUL) end is pointing to 
+		* into a char array called symbol
+		*/ 
+		char *hptr = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer
+		char symbol[2];
+		symbol[0] = *end; 
+		symbol[1] = '\0';
+		strcpy(hptr, symbol);
+
+		/*
+		* Add word and symbol to tokens_ds
+		*/
+		*end = '\0'; 					// Make end NUL to give the word a NUL terminator
+		char *hptr_beg = malloc(sizeof *hptr * 1);  	// hptr stands for heap pointer. NOTE: length = 1 here, this could cause problems
+		strcpy(hptr_beg, beg); 
+		tokens_ds[*rtc] = hptr_beg; 			// Add beg to tokens_ds
+		printf("rtc is %d\n", *rtc);
+		printf("Token added: %s\n", tokens_ds[*rtc]);
+		*rtc = *rtc + 1; 				// Increment rtc by 1
+		tokens_ds[*rtc] = hptr; 		// Add symbol to tokens_ds
+		printf("rtc is %d\n", *rtc);
+		printf("Token added: %s\n", tokens_ds[*rtc]);
+		*rtc = *rtc + 1; 				// Increment rtc by 1
+	}
 }
 
 
@@ -21,52 +61,55 @@ void tokenize_symbols(char ptr[], char *tokens_ds[], int *rtc)
 	/*
 	* Check if any of &, |, <, or > symbols exist
 	*/ 
+
 	if (strchr(ptr, '&') == NULL && strchr(ptr, '|') == NULL && strchr(ptr, '<') == NULL && strchr(ptr, '>') == NULL) {
 		// If none of the symbols exist, add ptr to tokens_ds and return
-		tokens_ds[*rtc] = ptr; 
+		char *hptr = malloc(sizeof *hptr * strlen(ptr));
+		strcpy(hptr, ptr); 
+		tokens_ds[*rtc] = hptr; 
+		printf("rtc is %d\n", *rtc);
+		printf("Token added: %s\n", tokens_ds[*rtc]);
 		*rtc = *rtc + 1; 		// Increment rtc by 1
 		return; 
 	} 
-	else { 		// If they do continue splitting
-		if (strchr(ptr, '&') != NULL) {
-			/* 
-			* Tokenize ptr for '&' and words
-			* Search using two-pointers algorithm (similar to strtok implementation under-the-hood)
-			*/ 
-			char *beg;		// Purpose of beg is to point to the beginning of a token
-			char *end; 		// Purpose of end is to point to the end of a token
-			beg = ptr; 
-			end = ptr; 
+	else { 		// At least one of the symbols &, |, <, or > exist
+		/* 
+		* Search ptr for words and special symbols: &, |, <, or >.
+		* Search using two-pointers algorithm (similar to C's strtok implementation)
+		*/ 
+		char *beg;		// Purpose of beg is to point to the beginning of a token
+		char *end; 		// Purpose of end is to point to the end of a token
+		beg = ptr; 
+		end = ptr; 
 
-			while (end != NULL) {
-				if (strcmp(end, "&") == 0 || strcmp(end, "|") == 0 || strcmp(end, "<") == 0 || strcmp(end, ">") == 0) {
-					add_to_tokens(beg, end, tokens_ds, rtc); 
-					beg = end + 1; 
-					end = end + 1; 
-				} else
-					end = end + 1; 
-			}
-
-			if (beg != NULL) {
+		while (*end != '\0') {
+			if (*end == '&' || *end == '|' || *end == '>' || *end == '<') {
+				// TODO: problem lies here. The program doesn't come here even though it should??
 				add_to_tokens(beg, end, tokens_ds, rtc); 
-			}
+				end = end + 1; 
+				beg = end; 
+			} else
+				end = end + 1; 
+		}
 
+		if (*beg != '\0') {
+			add_to_tokens(beg, end, tokens_ds, rtc); 
 		}
 
 	}
-
 }
 
 struct pipeline *pipeline_build(const char *command_line)
 {
 	const int cmdl_len = strlen(command_line); 	// Find size of command_line string
-	int rtc = 0; 	// Running tokens count, it is incremented after a token pointer is added to tokens_ds
+	int rtc = 0; 	// rtc stands for running tokens count, it increments by 1 after a token pointer is added to tokens_ds
 
 	/*
-	* Initialize array of char pointers (tokens_ds) to point to tokens
+	* Initialize array of char pointers (tokens_ds) to point to tokens and
+	* allocate maximum possible memory needed
 	*/
-	char **tokens_ds = (char **) malloc(cmdl_len * sizeof(char *)); // Allocate maximum possible memory needed
-
+	// char **tokens_ds = (char **) malloc(cmdl_len * sizeof(char *));
+	char **tokens_ds = malloc(sizeof **tokens_ds * cmdl_len);
 
 	/*
 	* Make non-const copy of command_line for the purpose of using with strtok which 
@@ -79,25 +122,37 @@ struct pipeline *pipeline_build(const char *command_line)
 	* Split cmdl_cpy by whitespace by calling strtok iteratively until the strtok's 
 	* next pointer points to NULL 
 	*/
-	char delim[] = " "; 						// Define delim to be " "	
+	char delim[] = " "; 					
 	char *ptr = strtok(cmdl_cpy, delim); 		// Make first call to strtok with parameters cmdl_cpy and delim 
 	while (ptr != NULL) {
 		/*
 		* Call tokenize_symbols on ptr which checks if any symbols &, |, <, or > exist
-		* in pt, and if so add them to tokens_ds
+		* in ptr, and if so add them to tokens_ds
 		*/
 		printf("%s\n", ptr);
 		char ptr_cpy[strlen(ptr)]; 
 		strcpy(ptr_cpy, ptr);
 		tokenize_symbols(ptr_cpy, tokens_ds, &rtc); 
+		printf("Token[0] added: %s\n", tokens_ds[0]);
+		printf("Token[1] added: %s\n", tokens_ds[1]);
+		printf("Token[2] added: %s\n", tokens_ds[2]);
 		ptr = strtok(NULL, delim);
 	}
 
+	printf("\n");
+	// printf("YASSS Token added: %s\n", tokens_ds[0]);
 	// for (int i = 0; i < sizeof(tokens_ds)/sizeof(tokens_ds[0]); i++) {
 	// 	printf("%s\n", tokens_ds[i]);
 	// }
-	printf("\n");
-	// Solve the parsing problem
+	// printf("\n");
+
+	/*
+	* Shrink size of tokens_ds
+	*/ 
+
+	/*
+	* Solve the parsing problem
+	*/ 
 	return NULL;
 }
 
@@ -106,18 +161,43 @@ void pipeline_free(struct pipeline *pipeline)
 	// TODO: Implement this function
 }
 
+void rahrah(char *tokens_ds[]) {
+	// char *ptr; 
+	char str[] = "jelly";
+	// ptr = str; 
+
+	char *ptr = malloc(sizeof *ptr * 1); 
+	strcpy(ptr, str);
+	tokens_ds[0] = ptr; 
+	printf("Inside rahrah: tokens_ds is %s\n", tokens_ds[0]); 
+}
+
+struct pipeline *pipeline_build_too(const char *command_line) {
+
+	return NULL; 
+}
 
 int main() {
 	// code here to test pipeline_build
-	// pipeline_build("julia  < zeng");
-	// pipeline_build("julia  zeng  ");
-	char *end;
+	// pipeline_build("julia <zeng"); 
+
+	// correct output: 
+	// julia
+	// <
+	// zeng
+
+	char **tokens_ds = malloc(sizeof **tokens_ds * 1);
+	char *ptr = malloc(sizeof *ptr * 1); 
 	char str[] = "julia"; 
-	end = str;
-	// if (strcmp(end, "julia") == 0) {
-	// 	printf("yes1\n");
-	// }
-	printf("%s\n", end);
+	strcpy(ptr, str);
+	printf("ptr is %s\n", ptr);
+	char str_too[] = "zeng";
+	strcpy(ptr, str_too);
+	printf("ptr is %s\n", ptr);
+	// printf("Before rahrah: tokens_ds is %s\n", tokens_ds[0]); 
+	// rahrah(tokens_ds);
+	// printf("After rahrah: tokens_ds is %s\n", tokens_ds[0]); 
+
 }
 
 
