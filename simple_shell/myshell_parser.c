9f133dc144a77ccae7e74cdd0be32416394b141d
jullianzz <jzeng8827@bths.edu>
2022-01-26 13:45:12 -0500

    Committing. Needs work: remove newline character.

--- simple_shell/myshell_parser.c
+++ simple_shell/myshell_parser.c
@@ -236,25 +236,26 @@ void pipeline_free(struct pipeline *pipeline)
 }
 
 
-int main() {
-	// code here to test pipeline_build
-	// struct pipeline *pipeline = pipeline_build("Julia |Zeng Chicken>"); // test case 1
-	// struct pipeline *pipeline = pipeline_build("Julia < Zeng < Chickens |yar");  // test case 2
-	// pipeline_build("Julia Zeng Chickens Yas"); // test case 3
-	// pipeline_build("Julia Zeng >Chickens |Yas"); // test case 4
-	// pipeline_build("Julia |Zeng Ch&icken>"); // test case 5
-	struct pipeline *pipeline = pipeline_build("ls|wc -l >counts.txt&"); // test case 6
+// int main() {
+// 	// code here to test pipeline_build
+// 	// struct pipeline *pipeline = pipeline_build("Julia |Zeng Chicken>"); // test case 1
+// 	// struct pipeline *pipeline = pipeline_build("Julia < Zeng < Chickens |yar");  // test case 2
+// 	// pipeline_build("Julia Zeng Chickens Yas"); // test case 3
+// 	// pipeline_build("Julia Zeng >Chickens |Yas"); // test case 4
+// 	// pipeline_build("Julia |Zeng Ch&icken>"); // test case 5
+// 	struct pipeline *pipeline = pipeline_build("ls|wc -l >counts.txt&\n"); // test case 6
 
-	printf("\nPRINT PIPELINE DATA STRUCTURE:\n");
-	printf("Is Background is: %d\n", pipeline->is_background);
-	printf("First word of first command is: %s\n", pipeline->commands->command_args[0]);
-	printf("Second word of first command is: %s\n", pipeline->commands->command_args[1]);
-	printf("First word of second command is: %s\n", pipeline->commands->next->command_args[0]);
-	printf("Second word of second command is: %s\n", pipeline->commands->next->command_args[1]);
-	printf("Third word of second command is: %s\n", pipeline->commands->next->command_args[2]);
-	printf("Pipe out of second command is: %s\n", pipeline->commands->next->redirect_out_path);
+// 	printf("\nPRINT PIPELINE DATA STRUCTURE:\n");
+// 	printf("Is Background is: %d\n", pipeline->is_background);
+// 	printf("First word of first command is: %s\n", pipeline->commands->command_args[0]);
+// 	printf("Second word of first command is: %s\n", pipeline->commands->command_args[1]);
+// 	printf("First word of second command is: %s\n", pipeline->commands->next->command_args[0]);
+// 	printf("Second word of second command is: %s\n", pipeline->commands->next->command_args[1]);
+// 	printf("Third word of second command is: %s\n", pipeline->commands->next->command_args[2]);
+// 	printf("Fourth word of second command is: %s\n", pipeline->commands->next->command_args[3]);
+// 	printf("Pipe out of second command is: %s\n", pipeline->commands->next->redirect_out_path);
 
-}
+// }
 
 
 
