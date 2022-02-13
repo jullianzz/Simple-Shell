b497efc02d565df38d543374c1ac247cb79c5de3
jullianzz <zjulia@bu.edu>
2022-02-13 22:45:32 +0000

    crash prevention

--- simple_shell/myshell.c
+++ simple_shell/myshell.c
@@ -8,8 +8,8 @@
 #include "sys/wait.h"
 #include "stdbool.h"
 
-bool rd_from_pipe = false; // rd_from_pipe is true for all commands in the pipeline, except for the first
-int rd_pipefd; // File descriptor of pipe file to read from for input to current command 
+bool rd_from_pipe;   // rd_from_pipe is true for all commands in the pipeline, except for the first
+int rd_pipefd;               // File descriptor of pipe file to read from for input to current command 
 
 /*
 * Point stdin and stdout to different files if input or output 
@@ -24,65 +24,65 @@ void setup_redirection(const struct pipeline_command *pcmd) {
     * If it does, open the file and use dup2() to write the
     * file contents into stdin. Then execute execv(). 
     */
-    int fd_in; 
-    if (pcmd->redirect_in_path != NULL) {  // Need to open redirect in file for reading
-        fd_in = open(pcmd->redirect_in_path, O_RDONLY); // Don't use O_RDONLY here
+    if (pcmd->redirect_in_path != NULL) {
+        int fd_in = open(pcmd->redirect_in_path, O_RDONLY); // Don't use O_RDONLY here
         dup2(fd_in, 0);  // Points stdin file descriptor to fd_in file
+        close(fd_in);    // Release fd_in file descriptor
     }
-    close(fd_in);     
     
     /*
     * Check if the pipeline_command contains a redirect out.
     * If it does, open the file and use dup2() to direct stdout 
     * of execv() to the file. 
     */
-    int fd_out;
-    if (pcmd->redirect_out_path != NULL) {  // Need to open redirect out file for writing 
-        fd_out = open(pcmd->redirect_out_path, O_WRONLY | O_CREAT | O_TRUNC); // Add O_CREAT flag if needed, need to check out specifications
+    if (pcmd->redirect_out_path != NULL) {
+        int fd_out = open(pcmd->redirect_out_path, O_WRONLY | O_CREAT | O_TRUNC);
         dup2(fd_out, 1);
+        close(fd_out);  // Release fd_out file descriptor
     }
-    close(fd_out);  // With dup2, stdout will be written to the file pointed to by fd. Can close fd.
+    
 }
 
-// Set up a new pipe for interprocess communication with the next pipeline command. 
+/*
+* Read from pipe file for input to current command, and create
+* a new pipe for interprocess communication with the next command. 
+*/ 
 void setup_pipe(const struct pipeline_command *pcmd) {
     /*
     * Check if the pcmd is either a middle or final command in the pipeline. 
-    * If pcmd is the first command in the pipeline, create a new pipe file,
+    * If pcmd is the first command in the pipeline, create a new pipe file, 
     * point stdout to new pipe WR FD, set rd_pipefd to new pipe RD FD,
     * and set rd_from_pipe to true. 
     * If pcmd is the middle or last command in the pipeline, point stdin to 
     * rd_pipefd, and do the same steps as the first command in the pipeline. 
     */
-
+   
     if (rd_from_pipe) {    /* pcmd is a middle or final command in the pipeline */ 
         dup2(rd_pipefd, 0);  // Point stdin to pipe file
         close(rd_pipefd);    // Release old pipe RD FD
+        printf("pcmd is middle or final cmd\n");
  
     } else {    /* pcmd is the first command in the pipeline */ 
         rd_from_pipe = true;   // Set rd_from_pipe to true to signal pipe read for next command
+        printf("pcmd is first cmd\n");
     }
-    
+    printf("pcmd is first cmd\n");
     int pipefd[2]; 
     pipe(pipefd); // Create a new pipe file, pipefd[0] = RD, pipefd[1] = WR
     rd_pipefd = pipefd[0];    // Set rd_pipefd to the new pipe RD FD
     dup2(pipefd[1], 1);     // Write the stdout of the command to the pipe file
-    close(pipefd[1]);       // Close the pipe WR FD because it is no longer needed
+    close(pipefd[1]);       // Release the pipe WR FD because it is no longer needed
     
 }
 
-
-// Call execute_cmds after pipeline_build
 void execute_cmds(const struct pipeline *pipeline) 
 {
     // printf("yass");
     int status;
-    // char *args[2];  // omit for now. using this would require copying from command_args array
-    // Simple design that does not account for redirection for now :  
-    // iterate through each pipeline_command in pipeline and execute each command using execve
     struct pipeline_command *pcmd; // Initialize pipeline_command pointer
     pcmd = pipeline->commands; // Point pcmd to first command in the linked list
-    bool path_specified = false; 
+    rd_from_pipe = false;   // Initialize rd_from_pipe signal
+    rd_pipefd = 0;          // Initialize rd_pipefd to avoid undefined behaviour
 
     while (pcmd != NULL) {
         pid_t pid = fork(); 
@@ -93,7 +93,7 @@ void execute_cmds(const struct pipeline *pipeline)
          
         } else if (pid == 0) { /* Child Process */  
             setup_redirection(pcmd); 
-//             setup_pipe(pcmd); 
+            setup_pipe(pcmd); 
             
             /*
             * Check if the first command args has a forward slash. If it does, it 
@@ -106,7 +106,7 @@ void execute_cmds(const struct pipeline *pipeline)
                 */
                 execvp(pcmd->command_args[0], pcmd->command_args);
             } else {
-                // Foward slash is present - use execv 
+                // Foward slash is present in command_args[0] - use execv 
                 execv(pcmd->command_args[0], pcmd->command_args);
             }
 
@@ -115,7 +115,6 @@ void execute_cmds(const struct pipeline *pipeline)
         } else {
             perror("Fork was unsuccessful");
             exit(EXIT_FAILURE);
-            // pid is negative and fork was unsuccessful
         }
     }
     exit(EXIT_SUCCESS);
@@ -128,7 +127,7 @@ int main(int argc, char *argv[]) {
 		// instantiate myshell 
 		// myshell(1); 
         // printf("hereee___");
-        struct pipeline *pb = pipeline_build("/bin/ls\n"); 
+//         struct pipeline *pb = pipeline_build("/bin/ls\n"); 
 //         struct pipeline *pb = pipeline_build("ls\n");
         // struct pipeline *pb = pipeline_build("ls > outfile.txt\n"); 
         // struct pipeline *pb = pipeline_build("ls -al | cat garbo_file.txt\n"); 
@@ -136,7 +135,7 @@ int main(int argc, char *argv[]) {
 //         struct pipeline *pb = pipeline_build("ls -al > outfile.txt | cat garbo_file.txt\n"); 
 //         struct pipeline *pb = pipeline_build("echo wowza > outfile.txt \n"); 
 //         struct pipeline *pb = pipeline_build("cat < garbo_file.txt > outfile.txt\n"); 
-//         struct pipeline *pb = pipeline_build("ls -al | wc -l\n"); // this command tests piping
+        struct pipeline *pb = pipeline_build("ls -al | wc -l\n"); // this command tests piping
         // printf("hereee***");
         execute_cmds(pb); 
         // printf("hereeeBLEEEEEP");
